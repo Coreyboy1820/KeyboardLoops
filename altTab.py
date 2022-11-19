56b3eaade1c42d025d58be291ab1fb5c69c26d31
coreyboy1820 <coreyboy1820@hotmail.com>
2022-11-19 02:21:09 -0800

    Finished AltTab loop to automatically iterate through tabs on the computer

---
+++ altTab.py
@@ -0,0 +1,17 @@
+import pyautogui as gui
+import time
+
+def AltTabLoop(tabs, count):
+    gui.keyDown('alt')
+    i=0
+    while i < count:
+        time.sleep(.05)
+        gui.press('tab')
+        time.sleep(.05)
+        i = i + 1
+    gui.keyUp('alt')
+    if count == (tabs-1):
+        count=1
+    else:
+        count=count+1
+    return tabs, count
---
+++ main.py
@@ -0,0 +1,32 @@
+import altTab
+import time
+import signal
+import os
+
+def SigIntHandler(signum, frame):
+    global toggle
+    print("You inputted signal number: ", signum, " Which was called in frame: ", frame)
+    if(toggle):
+        print("\nNo longer looping through tabs, press ctrl + C to resume.\n")
+    else:
+        print("\nNow looping through tabs, press ctrl + C to pause.\n")
+        print("To keep vscode out of the loop, alt tab all tabs until vscode is the last tab in the menu.\n")
+    print("To stop the program completely type:\nkill ", os.getpid(), "\nin the command prompt.")
+    if(toggle):
+        toggle=False
+    else:
+        toggle=True
+    
+def main():
+    signal.signal(signal.SIGINT, SigIntHandler)
+    global toggle
+    toggle=True
+    tabs=7
+    count=1
+    while(True):
+        if(toggle):
+            tabs, count = altTab.AltTabLoop(tabs, count)
+            print("Press ctrl + C to pause the program and see how to end this program")
+            time.sleep(10)
+
+main()
